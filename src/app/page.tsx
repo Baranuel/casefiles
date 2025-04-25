35f335cf631fc71185263e5ac061e369ff32165d
Samuel Baran <s.baran.dev@gmail.com>
2025-04-25 16:54:06 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn this no
+          Learn this
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
