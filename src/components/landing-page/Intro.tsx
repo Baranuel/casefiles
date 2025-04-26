47632f861b31d51b68343d8b403cbb365d02cc3e
Samuel Baran <s.baran.dev@gmail.com>
2025-04-26 11:29:41 +0200

    Update Intro.tsx

--- src/components/landing-page/Intro.tsx
+++ src/components/landing-page/Intro.tsx
@@ -3,7 +3,7 @@ import { SectionHeader } from "../global/SectionHeader"
 export default function Intro() {
   return (
     <section className="w-full min-h-[350px] bg-primary-700/5">
-      <div className="w-full flex flex-col items-center gaplg:justify-center mx-auto px-4 lg:px-20 py-12 lg:py-24">
+      <div className="w-full flex flex-col items-center lg:justify-center mx-auto px-4 lg:px-20 py-12 lg:py-24">
           <SectionHeader>
           <span className="bg-gradient-to-r from-primary-600 to-primary-700 bg-clip-text text-transparent">
             Introduction
