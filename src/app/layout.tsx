3b50d735eb740f1089adf201a8791b171b488bb5
Samuel Baran <s.baran.dev@gmail.com>
2025-04-27 09:47:59 +0200

    dynamic maybe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider publishableKey={'pk_test_YWRqdXN0ZWQtbWFja2VyZWwtODcuY2xlcmsuYWNjb3VudHMuZGV2JA'}>
+    <ClerkProvider dynamic publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || ''}>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
