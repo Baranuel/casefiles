f8daa827b632b9046b14c539da6c8a8063ccc0f1
Samuel Baran <s.baran.dev@gmail.com>
2025-04-28 17:18:55 +0200

    Add pricing page

--- src/components/landing-page/Intro.tsx
+++ src/components/landing-page/Intro.tsx
@@ -3,14 +3,14 @@ import { SectionWrapper } from "../global/SectionWrapper";
 
 export default function Intro() {
   return (
-    <section className="bg-primary-700/4">
+    <section className="bg-gradient-to-b from-primary-700/5 to-transparent">
       <SectionWrapper className="items-center">
         <SectionHeader>
           <span className="bg-gradient-to-r from-primary-600 to-primary-700 bg-clip-text text-transparent">
             Introduction{" "}
           </span>
         </SectionHeader>
-        <div className=" w-full min-h-[320px] aspect-video  lg:max-w-4/5 bg-black rounded-lg shadow-xl">
+        <div className=" w-full min-h-[250px] aspect-video  lg:max-w-4/5 bg-black rounded-lg shadow-xl">
           <iframe
             className="w-full h-full rounded-lg"
             title="Introduction Video"
